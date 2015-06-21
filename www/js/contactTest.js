8aff0f334d4974e5485709128cde5e45fcab13f2
Felix Liu <felix_liu@outlook.com>
2015-06-21 10:26:02 +0800

    Test geolocation plugin

--- www/js/contactTest.js
+++ www/js/contactTest.js
@@ -9,26 +9,32 @@ if (myapp === undefined) {
    
 myapp.controller('bindControl', function ($http, $timeout) {
     'use strict';
-//    var bindCtrl = this;
-//
-//    document.addEventListener("deviceready", function () {
-//        $cordovaGeolocation.getCurrentPosition({
-//            timeout : Number(window.localStorage.timeOut),
-//            enableHighAccuracy : true
-//        }).then(function (position) {
-//            var lat  = position.coords.latitude,
-//                long = position.coords.longitude;
-//            alert('lat =',lat,'long =',long);
+    var bindCtrl = this,
+        onSuccess = function(position) {
+        alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+    
+    document.addEventListener("deviceready", function () {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+//        navigator.contacts.find(['id', 'displayName'], function (contacts) {
+//            alert('Found ' + contacts.length + ' contacts.');
 //        }, function (err) {
 //            alert(err);
 //        });
-//    }, false);
-    
-    document.addEventListener("deviceready", function () {
-        navigator.contacts.find(['id', 'displayName'], function (contacts) {
-            alert('Found ' + contacts.length + ' contacts.');
-        }, function (err) {
-            alert(err);
-        });
     }, false);
 });
